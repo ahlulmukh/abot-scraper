5a430f90204494eaa9aaa834d1c073080ffafd54
Ahlul Mukhramin <ahlul963@gmail.com>
2024-01-18 16:19:46 +0700

    update ytplay

--- example/search.js
+++ example/search.js
@@ -1,6 +1,14 @@
 const { ytPlay, wallpaper } = require("../src");
 
-wallpaper("Naruto")
+// wallpaper("Naruto")
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+ytPlay("Abadi Speedup")
   .then((result) => {
     console.log(result);
   })
